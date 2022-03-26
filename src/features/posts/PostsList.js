6336ff5977ed452ce2132dcac6c89bf1251a50fc
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-03-26 21:26:56 +0100

    03

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -7,6 +7,6 @@ export const PostsList =()=>{
 	const renderedPosts = posts.map(post=>(
 		<article className="post-excerpt" key={post.id}>
 		  <h3>{post.title}</h3>
-
+          <p className=
 		  ))
 }
