16c9c9e449417c3d740ebaa6d26d367bb61b4157
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-03-27 23:20:49 +0200

    67

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import {useSelector} from 'react-redux';
+import {Link} from 'react-router-dom';
 
 export const PostsList = ()=>{
 	const posts = useSelector(state=>state.posts)
 
-	const renderedPosts = posts.map(post =>(
-      <article className="post-excerpt" key={post.id}>
-        <h3>{post.title}</h3>
-        <p className="post-content">{post.content.substring(0,100)}</p>
+const renderedPosts = posts.map(post =>(
+  <article className="post-excerpt" key={post.id}>
+
+    <h3>{post.title}</h3>
+
+<p className="post-content">
+           {post.content.substring(0,100)}</p>
+
+<Link to={`/posts/${post.id}`} 
+      className="button muted-button">
+ View Post
+    </Link>
        </article>
 		))
 
