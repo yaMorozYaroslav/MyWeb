5de6b434ba73502acf131ab7398461b6c26a0351
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-03-28 14:44:25 +0200

    94

--- src/features/posts/EditPostForm.js
+++ src/features/posts/EditPostForm.js
@@ -12,6 +12,38 @@ export const EditPostForm = ({match})=>{
     const [title, setTitle] = useState(post.title)
     const [content, setContent] = useState(post.content)
 
-    const dispatch
+    const dispatch = useDispatch()
+    const history = useHistory()
+
+    const onTitleChange = e => setTitle(e.target.value)
+    const onContentChange = e => setContent(e.target.value)
+
+    const onSavePostClicked =()=>{
+    	if(title&&content){
+    		dispatch(postUpdated({id: postId, title, content}))
+    		history.push(`/posts/${postId}`)
+    	}
+    }
+
+    return (
+  <section>
+     <h2>Edit Post</h2>
+       <form>
+         <label htmlFor="postTitle">Post Title: </label>
+         <input 
+                type="text"
+                id="postTitle"
+                name="postTitle"
+                placeholder="Success is on my mind."
+                value={title}
+                onChange={onTitleChanged}
+                />
+              </form>
+            <button type="button" onClick={onSavePostClicked}>
+               Save Post
+              </button>
+             </section>
+    	)
+     }
 
 }
