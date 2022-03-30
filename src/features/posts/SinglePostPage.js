e0b347ad3d544dd293878f39919868e748323656
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-03-30 15:32:13 +0200

    84

--- src/features/posts/SinglePostPage.js
+++ src/features/posts/SinglePostPage.js
@@ -1,25 +1,22 @@
-import React from 'react';
+import React from 'react'
 import {useSelector} from 'react-redux'
 
-export const SinglePostPage = ({match})=>{
+export const SinglePostPage = ({match}) =>{
 	const {postId} = match.params
 
-	const post = useSelector(state=>
-  state.posts.find(post=>post.id===postId)
-		)
-	if (!post){
-		return( 
-     <section>
-        <h2.POst not found!</h2>
-       </section>
-			)
-	    }
+	const post = useSelector(
+       state=>post.state.find(
+       	      post=>post.id===postId))
+
+	if(!post){return(<section><h2>Not Found</h2></section>)}
+
     return(
-  <section>
-    <article className="post">
-      <h2>{post.title}</h2>
-      <p classname="post-content">{post.content}</p>
-     </article>
-    </section>
-  )
-}
+
+     <section>
+      <article className="post">
+           <h2>{post.title}</h2> <p className="post-content">{post.content}</p>
+      </article>
+     </section>
+
+          )
+                                          }
