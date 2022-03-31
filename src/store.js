8e69f5f3279808c782e643cf884a5b96c8412bf6
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-03-31 14:33:44 +0200

    48

---
+++ src/store.js
@@ -0,0 +1,6 @@
+import {configureStore} from '@reduxjs/toolkit'
+import postsReducer from '../features/posts/postsSlice'
+
+export default configureStore({
+	                           reducer:{}
+                                         })
