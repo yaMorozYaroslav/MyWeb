001b5bf6a79485c7a1b3c511ccdf23bb78a0ddd4
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-04-03 19:06:50 +0200

    33

---
+++ src/features/users/usersSlice.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {createSlice} from '@reduxjs/toolkit'
+
+
+	const initialState=[{id: "0", name: "User"},{id: "1", name: "Vanya"}]
+    
+    const usersSlice = createSlice({
+    	name: "users",
+    	initialState,
+    	reducers: {}
+    })
+export default usersSlice.reducer
