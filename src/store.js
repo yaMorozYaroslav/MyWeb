e4e4fa35a52b21eeca34a1bd8962486d66503d54
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-04-03 16:26:53 +0200

    33

--- src/store.js
+++ src/store.js
@@ -1,10 +1,9 @@
 import {configureStore} from '@reduxjs/toolkit'
-import postsReducer from './features/posts/postsSlice'
 import nodesReducer from './features/node/nodeSlc'
 
 export default configureStore({
 	                           reducer:{
-	                           	   posts: postsReducer,
+	                       
 	                           	   nodes: nodesReducer
 	                                     }
                                          })
