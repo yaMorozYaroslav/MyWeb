e910fa7b1ed011ac0ff586cb4ebe158554e84f58
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-04-09 15:45:52 +0200

    95

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -1,16 +1,35 @@
 import React, {useEffect} from 'react'
 import {useSelector, useDispatch} from 'react-redux'
 import {Link} from 'react-router-dom'
+import {Spinner} from '../../components/Spinner'
 import {Author} from './Author'
 import {TimeAgo} from './TimeAgo'
 
 import {selectAllPosts, fetchPosts} from './postsSlice'
+
+const PostExcerpt = ({post})=>{
+  return(
+    <article className="post-excerpt" key={post.id}>
+       <h3>{post.title}</h3>
+       <div>
+          <Author userId={post.user} />
+          <TimeAgo timestamp={post.date}/>
+        </div>
+      <p className="post-content">{post.content.substring(0, 100)}</p>
+    
+     <Link to={`/posts/${post.id}`} className="button muted-button">
+       View Post
+     </Link>
+    </article>
+    )
+}
 
 export const PostsList =()=> {
   const dispatch = useDispatch()
 	const posts = useSelector(selectAllPosts)
 
 const postStatus = useSelector(state=>state.posts.status)
+const error = useSelector(state=>state.posts.error)
 
     useEffect(()=>{
       if(postStatus==='idle'){
@@ -18,22 +37,25 @@ const postStatus = useSelector(state=>state.posts.status)
       }
     }, [postStatus, dispatch])
 
-  const orderedPosts = posts.slice().sort((a, b)=>b.date.localeCompare(a.date))
+ let content
 
-	const renderedPosts = orderedPosts.map(post=>(
+ if(postStatus === 'loading'){
+  content = <Spinner text="Loading..."/>
+ } else if (postStatus === 'succeeded'){
+  const orderedPosts = posts
+       .slice()
+       .sort((a,b)=>b.date.localeCompare(a.date))
 
-      <article className="excerpt" key={post.id}>
-                                  
-                   <h3>{post.title}</h3> 
-                   <p className="post-content">{post.content.substring(0,100)}</p>
-                   <TimeAgo timestamp={post.date}/>
-                   <Author userId={post.user} /><br/>
-            <Link to={`posts/${post.id}`} className="button muted-button">
-                                                                 View Post</Link>
-      </article>
-	))
-	  return (
-    <section className="posts-list"><h1>Posts</h1>{renderedPosts}
-      </section>
-	  )
-    }
+       content = orderedPosts.map(post=>(
+        <PostExcerpt key={post.id} post={post}/>
+        ))
+      }else if (postStatus === 'failed'){
+        content = <div>{error}</div>
+      }
+return (
+    <section className="posts-list">
+      <h2>Posts</h2>
+      {content}
+    </section>
+)
+}
