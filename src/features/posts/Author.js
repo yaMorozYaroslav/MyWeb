61f48c8d4deecad45c0e1f587367b09db51ea269
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-04-09 19:41:48 +0200

    48

--- src/features/posts/Author.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import {useSelector} from 'react-redux'
-
-export const Author = ({userId}) => {
-	const author = useSelector(state=>
-		state.users.find(user=>user.id===userId))
-	return <span>by {author? author.name:'Unknown'}</span>
-}
