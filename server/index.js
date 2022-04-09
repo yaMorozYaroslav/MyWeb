3cccdabdcd4bb74bad74a38550dccfbbcfa8ed37
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-04-09 22:25:23 +0200

    32

--- server/index.js
+++ server/index.js
@@ -1,4 +1,20 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
+import express from 'express'
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
+import cors from 'cors'
+
+import postRoutes from './routes/posts.js'
+
+const app = express()
+
+app.use('/posts', postRoutes)
+app.use(bodyParser.json({limit: "30mb", extended: true}))
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}))
+app.use(cors())
+
+const CONNECTION_URL = 'mongodb+srv://Yaroslav:12345@cluster0.tlfsw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const PORT = process.env.PORT||5000
+
+mongoose.connect(CONNECTION_URL)
+ .then(()=>app.listen(PORT, ()=>console.log(`Running on ${PORT}`)))
+ .catch((error)=>console.log(error.message))
