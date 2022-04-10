f2cbaa2c57ef648313c4b84e505ceea2226cb5ad
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-04-10 15:35:54 +0200

    18

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
+  const Func=({numbers})=>{
+  	let place = []
+    for(let number of numbers){
+    	if(number %2 !== 0){
+    		place.push(number)
+    	}
+      }
+            
+          	return place;
+  }
+
 export const App =()=>{
+	let numbers = [1,2,3,4,5,6,7]
 	return(
-      <div>
-          <h1>App</h1>
-      </div>
+      <Func numbers={numbers}/>
 	);
 }
--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,8 @@
+import PostMessage from '../models/postMessage.js'
+
 export const getPosts=(req,res)=>{
 	res.send('THIS WORKS!');
+}
+export const createPost = (req,res)=>{
+	res.send('Post Creation')
 }
