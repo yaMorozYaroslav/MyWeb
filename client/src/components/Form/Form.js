0fcbd64f47929ea28a74e66f586d20caba0ec44b
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-04-11 22:11:59 +0200

    32

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,15 +1,21 @@
 import React from 'react'
 import {TextField, Button, Typography, Paper} from '@material-ui/core'
 import FileBase from 'react-file-base64'
+import {useDispatch} from 'react-redux'
+
 import useStyles from './styles'
+import {createPost} from '../../actions/postact'
 
 export const Form =()=>{
 	const [postData, setPostData] = React.useState({
 	creator: '', title: '', message: '', tags: '', selectedFile: ''
 	})
 	const classes = useStyles()
-	const handleSubmit =()=>{
+    const dispatch = useDispatch()
 
+	const handleSubmit =(e)=>{
+       e.preventDefault()
+       dispatch(createPost(postData))
 	}
 	const clear =()=>{
 
