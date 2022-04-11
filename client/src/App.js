97fb66da6ed4b1ce9881e6f98364a806e7730ba5
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-04-11 14:27:52 +0200

    88

--- client/src/App.js
+++ client/src/App.js
@@ -13,10 +13,11 @@ export const App =()=> {
           className={classes.appBar}
           position="static"
           color="inherit">
-           <Typography 
-                   className={classes.heading}
-                   variant="h2" 
-                   align="center">
+          
+        <Typography 
+             className={classes.heading}
+             variant="h2" 
+             align="center">
                                Memories</Typography>
            <img
                className={classes.image}
--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,5 +1,7 @@
 import React from 'react'
+import useStyles from './styles'
 
 export const Form =()=>{
+	const classes = useStyles()
 	return(<h1>Form</h1>)
 }
