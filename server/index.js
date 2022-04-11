9cdd0d063b91d43d22f6f7c4f0d131bd8ce0bb5a
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-04-11 14:40:02 +0200

    49

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json({limit: "30mb", extended: true}))
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}))
 app.use(cors())
 
-const CONNECTION_URL = 'mongodb+srv://Yaroslav:12345@cluster0.tlfsw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const CONNECTION_URL = 'mongodb+srv://Yaroslav:<password>@cluster0.tlfsw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const PORT = process.env.PORT||5000
 
 mongoose.connect(CONNECTION_URL)
