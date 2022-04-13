9d39766077fa7963403bd052410de51511739cfb
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-04-13 19:55:23 +0200

    47

--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
+import dotenv from 'dotenv'
 
 import postRoutes from './routes/posts.js'
 
 const app = express()
+dotenv.config()
 
 app.use(bodyParser.json({limit: "30mb", extended: true}))
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}))
@@ -13,9 +15,12 @@ app.use(cors())
 
 app.use('/posts', postRoutes)
 
-const CONNECTION_URL = 'mongodb+srv://Yaroslav:12345@cluster0.tlfsw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+app.get('/', (req,res)=>{
+    res.send('Hello to Memories API')
+})
+
 const PORT = process.env.PORT||5000
 
-mongoose.connect(CONNECTION_URL)
+mongoose.connect(process.env.CONNECTION_URL)
  .then(()=>app.listen(PORT, ()=>console.log(`Running on ${PORT}`)))
  .catch((error)=>console.log(error.message))
