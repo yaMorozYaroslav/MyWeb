1209b6615202af8d182488eda58608c3be302e76
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-04-13 19:30:40 +0200

    466

--- client/src/styles.js
+++ client/src/styles.js
@@ -1,6 +1,6 @@
 import {makeStyles} from '@material-ui/core/styles'
 
-export default makeStyles(()=>({
+export default makeStyles((theme)=>({
 	appBar:{
 		borderRadius: 15,
 		margin: '30px 0',
@@ -16,5 +16,11 @@ export default makeStyles(()=>({
 		marginLeft: '15px',
 		width: '80px',
 		height: '80px'
+	},
+
+	[theme.breakpoints.down('sm')]: {
+		mainContainer: {
+			flexDirection: "column-reverse"
+		}
 	}
 }))
