f2944d367480b8c7899716d4d3eda15d4bfab2fd
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-04-13 22:39:33 +0200

    make

--- client/src/reducers/postred.js
+++ client/src/reducers/postred.js
@@ -2,7 +2,7 @@ import {FETCH_ALL, CREATE, UPDATE, DELETE} from '../constants/actionTypes'
 
 export default(posts=[], action)=>{
 	switch(action.type){
-	case DELET:
+	case DELETE:
 	    return posts.filter((post)=>post._id !== action.payload)
 	case UPDATE:
 	    return posts.map((post)=>post._id===action.payload._id
