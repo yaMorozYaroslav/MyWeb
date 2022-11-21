f1c7280775fffaeaf1c501cb05e2ed45cfc7cef9
Yaroslav Moroz <ya.moroz.yaroslav@gmail.com>
2022-11-21 08:53:43 -0400

    848

--- src/App/App.js
+++ src/App/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import s from './App.module.css'
+import {Header} from '../comps/Header/Header'
 
 export function App() {
   return (
     <div className={s.body}>
-       <div className={s.header}>Header</div>
+       <section className={s.header}><Header /></section>
        
     </div>
   )
--- src/comps/Header/Header.js
+++ src/comps/Header/Header.js
@@ -9,7 +9,5 @@ export const Header =()=> {
 	                           </div>
 	                           <div className={s.second} />
 	                           </div>)
-	                            
-	                
-	    )
+	              
 	}
