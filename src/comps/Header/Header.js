a4e5b2bc26865ea39bf59846bff0d752e14a496c
Yaroslav Moroz <ya.moroz.yaroslav@gmail.com>
2022-11-21 23:39:40 -0400

    048

--- src/comps/Header/Header.js
+++ src/comps/Header/Header.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import s from './Header.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGear } from '@fortawesome/free-solid-svg-icons'
 
 export const Header =()=> {
 	    return ( <div className={s.header}>
+	    
 	                          <div className={s.first}>
 	                             <h1 className={s.text}>Development</h1>
-	                             <div className={s.gear}><i className="fa fa-gear fa-3x"></i></div>
+	                             <div className={s.gear}>Text<i className = 'fa-solid fa-gears fa-3x'></i></div>
+	                             <FontAwesomeIcon icon={faGear}/>
 	                           </div>
 	                           <div className={s.second} />
 	                           </div>)
