58402096059f1f45a754042acf5a7570c54c0542
Yaroslav Moroz <ya.moroz.yaroslav@gmail.com>
2022-11-30 23:45:43 -0400

    039

--- src/Comps/Profile/Profile.js
+++ src/Comps/Profile/Profile.js
@@ -4,23 +4,28 @@ import photo from './myPhoto.jpg';
 
 export const Profile = () => {
 	return <>
-	                 <div  className={styles.container}> 
+	            
+	  <div  className={styles.container}> 
+		<div className={styles.text}>
+		
+		   <img className={styles.photo} src={photo} alt="yaro"/>
+		   
+		   <h1 className={styles.name}> Jaroslav Moroz, 23 years old</h1>
+		 
+        <section>I'm a self-taught NodeJS developer who explores the entire stack
+	 to understand the overall development process. Being able to use 
+	 JS both on the server side and on the client side inspires me 
+     to explore all the complexity by focusing on one language. </section><br/>
      
-	    
-		<p className={styles.text}>
-		<img className={styles.photo} src={photo} alt="yaro"/>
-		 I am a self-taught NodeJS developer<br/> who explores the
-	 entire stack to understand the overall
-	 development process. The ability to use JS for both the 
-	 backend and the frontend inspires me to do this, because <br/>I 
-	 can dive deeper while focusing on one language. Mainly 
-	 interested in creating websites and applications. In the 
-	 future, I wanna participate in the creation of AI web 
+        I installed the Arch Linux distribution with the i3 window manager.
+     on both of my laptops to take advantage of its performance
+     and partitioned the hard drive of one of them to play games on Win 11.<br/>
+		  
+      In the future, I wanna participate in the creation of AI web 
 	 applications. It is a long way, but why not if there
 	 is a lifetime. Waiting for new acquaintances and profitable
-	  offers.</p>
-     <p className={styles.name}>
-	    Jaroslav Moroz, 23 years old</p>
-	  </div>
+	  offers.
+	     </div>
+	   </div>
 	 </>;
 }
--- src/Comps/Project/Project.js
+++ src/Comps/Project/Project.js
@@ -2,5 +2,7 @@ import React from 'react'
 import s from './Project.module.css'
 import itemImg from './itemImg.png'
 export function Project(){
-	return (<><div  className={s.container} ><img src={itemImg} alt="itemImg" /></div></>)
+	return (<><div  className={s.container} >
+	<img className={s.img} src={itemImg} alt="itemImg" />
+	</div></>)
 	}
