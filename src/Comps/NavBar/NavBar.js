b29486f4d5fb698575a42ae1ff6847c04bea5eda
Yaroslav Moroz <ya.moroz.yaroslav@gmail.com>
2022-12-02 08:53:06 -0400

    945

---
+++ src/Comps/NavBar/NavBar.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+import s from'./NavBar.module.css';
+import {useLocation} from "react-router-dom";
+
+import {useDispatch, useSelector} from 'react-redux'
+import {closeContact, openContact} from '../../Redux/openSlice'
+
+export const NavBar = () => {
+	const location = useLocation();
+	const {pathname} = location;
+	const splitLocation = pathname.split("/")
+	
+	const dispatch = useDispatch()
+	const selectContact = state => state.open.contact
+	const contact = useSelector(selectContact)
+	
+	React.useEffect(()=>{
+	   if(splitLocation[1]==="contacts") dispatch(openContact()) 
+	   if(splitLocation[1]!=="contacts" && !contact) dispatch(closeContact())
+		},[location, contact, dispatch, splitLocation])
+	React.useEffect(()=>{
+	     console.log(contact)
+	    //if(contact === true) console.log(contact)
+		},[contact])
+	
+	return(
+		<>
+		  <nav className={s.nav}>
+		  
+		      <button className={splitLocation[1]===""?s.active:s.idle}>
+		        <Link  className={s.link} to="/">Intro </Link>
+		      </button>
+		      
+		      <button className={splitLocation[1]==="profile"?s.active:s.idle}>
+		        <Link className={s.link} to="/profile">Profile</Link>
+		      </button>
+		      
+		       <button className={splitLocation[1]==="project"?s.active:s.idle}>
+		          <Link className={s.link} to="/project">Projects</Link>
+		     </button >
+		     
+		     <button className={splitLocation[1]==="contacts"?s.active:s.idle}>
+		          <Link className={s.link}  to="/contacts">Contacts</Link>
+		     </button>
+		     
+		    </nav>
+		   </>)
+	   }
