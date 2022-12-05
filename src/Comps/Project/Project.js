84588306cfee3b5bb302b11a87c255c1c2244a12
Yaroslav Moroz <ya.moroz.yaroslav@gmail.com>
2022-12-05 08:48:27 -0400

    927

--- src/Comps/Project/Project.js
+++ src/Comps/Project/Project.js
@@ -4,5 +4,10 @@ import itemImg from './itemImg.png'
 export function Project(){
 	return (<><div  className={s.container} >
 	<img className={s.img} src={itemImg} alt="itemImg" />
+	      This is one of my first full stack projects created half a year ago
+	    using NodeJS, ExpressJS, MongoDB, React and Redux.
+	      The application has stages of registration and authorization,
+       after which the user is allowed to add items to the database
+       and manage the ones he added.
 	</div></>)
 	}
