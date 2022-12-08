bdcb7f9b125f1c102f85b146b3a7882b2d0da8c6
Yaroslav Moroz <ya.moroz.yaroslav@gmail.com>
2022-12-08 08:55:03 -0400

    88#

--- src/Comps/Project/Project.js
+++ src/Comps/Project/Project.js
@@ -6,8 +6,10 @@ export function Project(){
 	<img className={s.img} src={itemImg} alt="itemImg" />
 	      This is one of my first full stack projects created half a year ago
 	    using NodeJS, ExpressJS, MongoDB, React and Redux.
-	      The application has stages of registration and authorization,
-       after which the user is allowed to add items to the database
-       and manage the ones he added.
+	      
+	      There is a registration that allows you to store the name, 
+	      email address and encrypted password in MongoDB for 
+	      later use in token authentication. An authorized user can 
+	      add items to the database and manage those that he added.
 	</div></>)
 	}
