91bb62659a55e35a6d72743cb18b4603bcc514b4
Yaroslav Moroz <ya.moroz.yaroslav@gmail.com>
2022-12-14 08:54:41 -0400

    858

--- src/App/App.js
+++ src/App/App.js
@@ -9,12 +9,20 @@ export function App() {
 	
 const selectSection = state => state.open.section
 const section = useSelector(selectSection)
-//console.log(section)
+
+	/* let background;
+	if(section === 'intro')background = 's.backIntro'
+	if(section === 'profile')background='s.backProfile'
+console.log(background)
+React.useEffect(()=>{
+
+	},[]) */
   return (
     <div className={s.body}>
+      <div className={section==='intro'?s.backIntro:null}>
        <section className={s.header}><Header /></section>
        <section className={s.router}><Router/></section>
-      
+      </div>
     </div>
   )
 }
