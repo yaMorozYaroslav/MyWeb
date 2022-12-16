b4a791406c7dd28c91fee530102f0ef8a7db9a37
Yaroslav Moroz <ya.moroz.yaroslav@gmail.com>
2022-12-16 08:43:28 -0400

    002

--- src/Comps/Profile/Profile.js
+++ src/Comps/Profile/Profile.js
@@ -5,8 +5,7 @@ import photo from './myPhoto.jpg';
 export const Profile = () => {
 	return <>
 	            
-	  <div  cla         and other resources.
-ssName={s.container}> 
+	  <div  className={s.container}> 
 		
 		   <div className={s.pic}/>
 		   
@@ -17,17 +16,16 @@ ssName={s.container}>
           who learned how to build React apps
           using documentation and other online sources.
           
-          Being able to use 
-	 JS both on the server side and on the client side inspires me 
-     to explore all the complexity by focusing on one language. <br/>
+        Being able to use JS as a back-end inspired me
+         to create a REST API which allows authorization with ExpressJS using MongoDB
+          <br/>
      
-        <section>I installed the Arch Linux distribution with the i3 window manager.
+        <section>I installed the Arch Linux distribution with the i3 window manager
      on both of my laptops to take advantage of its performance
      and partitioned the hard drive of one of them to play games on Win 11.</section><br/>
 		  
-      In the future, I wanna participate in the creation of AI web 
-	 applications. It is a long way, but why not if there
-	 is a lifetime. Waiting for new acquaintances and profitable
+       I wanna participate in the creation of AI web 
+	 applications. Waiting for new acquaintances and profitable
 	  offers.
 	        </section>
 	     </div>
