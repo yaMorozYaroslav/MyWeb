34a0425b13c96809a15ffb63cef833d9856b1dc3
Yaroslav Moroz <ya.moroz.yaroslav@gmail.com>
2022-12-26 16:09:08 -0400

    7721

--- src/Comps/Email/Email.js
+++ src/Comps/Email/Email.js
@@ -32,27 +32,28 @@ export const Email = () => {
 	 //{alert? <Alert /> : null}
 	return(
 	<section className={s.container}>
-	      <button onClick={()=>dispatch(closeEmail())}>X</button>
-	       <h2 className={s.tit}>Contact me</h2>
 	      <div className={s.form}>
 	        <form ref={form} onSubmit={sendEmail}>
-	             <label  className={s.label} for='Name'>Name</label>
+	             <label  className={s.label} htmlFor='Name'>Name</label>
 	             <input className={s.input}
 	                                         type='text' placeholder='Name' name='user' required/>
-	              <label  className={s.label} for='Email'>Email</label>
+	              <label  className={s.label} htmlFor='Email'>Email</label>
 	             <input className={s.input}
 	                                         type='email' placeholder='Email' name='userEmail' required/>
-	             <label  className={s.label} for='Subject'>Subject</label>
+	             <label  className={s.label} htmlFor='Subject'>Subject</label>
 	             <input className={s.input} 
 	                                         type='text' placeholder='Subject' name='subject' required />
 	   	             <h1 className={s.subtit}>Message</h1>  
 	             <textarea className={s.textarea}
 	                                         name='message' cols='30' rows='5'></textarea>
 	              <button className={s.button} type='submit' onClick={()=>dispatch(openAlert())}>
-	              Send Message</button>
+	              Send</button>
 	         </form>
 	       </div>
-	      
+	       <div>
+	         <button className={s.closer} onClick={()=>dispatch(closeEmail())}>X</button>
+	         <h2 className={s.tit}>Contact Form</h2>
+	       </div>
 	     </section>                                         
 	)	
 }
