d37a3ed17987640cbd10dfb7e80c74de302897ca
Yaroslav Moroz <ya.moroz.yaroslav@gmail.com>
2022-12-29 22:34:48 -0400

    0032

--- src/Comps/Footer/Footer.js
+++ src/Comps/Footer/Footer.js
@@ -2,5 +2,8 @@ import React from 'react'
 import s from './Footer.module.css'
 
 export const Footer = () => {
-	return <div className={s.container}>©2023 All Rights Reserved</div>
+	return (<div className={s.container}>
+	<section className={s.author}> ©Yaroslav Moroz, 2023 </section>
+	<section className={s.rights}>All Rights Reserved</section>
+	              </div>)
 	}
