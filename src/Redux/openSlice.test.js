9a2d06fef0cb24ce32c0fb2c72062516d3f8ce8d
Yaroslav Moroz <ya.moroz.yaroslav@gmail.com>
2023-01-29 10:39:07 -0400

    482

---
+++ src/Redux/openSlice.test.js
@@ -0,0 +1,5 @@
+import openReducer, {openAlert, closeAlert} from './openSlice'
+
+test('should return the initial state', () => {
+	
+	})
--- src/Redux/store.js
+++ src/Redux/store.js
@@ -2,9 +2,9 @@ import {configureStore} from '@reduxjs/toolkit';
 
 import openReducer from './openSlice'
 
-export const store = configureStore({
-	reducer: {
-		      open: openReducer
-		}
+export const store = preloadedState => {
+	return configureStore({
+	reducer: { open: openReducer, preloadedState})
 		
-	})
+	 })
+   }
