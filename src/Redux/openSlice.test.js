75bc9db97cee4c686dbfce5de4f2016f4a8e6baf
Yaroslav Moroz <ya.moroz.yaroslav@gmail.com>
2023-01-30 08:44:57 -0400

    001

---
+++ src/Redux/openSlice.test.js
@@ -0,0 +1,23 @@
+import '@testing-library/jest-dom'
+import {setupStore} from './store'
+import openReducer, {openAlert, closeAlert} from './openSlice'
+
+const store = setupStore()
+
+test('should return the expected initial state', () => {
+	const expected = {alert: false, email: false, section: 'intro'}
+	const actual = store.getState().open
+	expect(actual).toEqual(expected)
+	})
+test('should change the alarm value to true',() => {
+	const expected = {alert: true}
+	store.dispatch(openAlert())
+	const actual = store.getState().open.alert
+	expect(actual).toBe(true)
+	})
+test('should change the alarm value to false',() => {
+	const expected = {alert: false}
+	store.dispatch(closeAlert())
+	const actual = store.getState().open.alert
+	expect(actual).toBe(false)
+	})
