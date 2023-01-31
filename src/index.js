4651c811f5ddc0e3c72f64468abb2da9c5cd68e4
Yaroslav Moroz <ya.moroz.yaroslav@gmail.com>
2023-01-30 22:35:37 -0400

    005

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react'
 import {createRoot} from 'react-dom/client'
 import {App} from './App/App'
 import {Provider} from 'react-redux'
-import {store} from './Redux/store'
+import {setupStore} from './Redux/store'
 
 const rootElement = document.getElementById('root')
 const root = createRoot(rootElement)
 
 root.render(
-  <Provider store={store}>
+  <Provider store={setupStore()}>
     <App />
   </Provider>
 );
