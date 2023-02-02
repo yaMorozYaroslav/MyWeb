037acf0a862d259cbaf8b3288d423639d6961c64
Yaroslav Moroz <ya.moroz.yaroslav@gmail.com>
2023-02-01 22:33:04 -0400

    009

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,9 +1,41 @@
 import '@testing-library/jest-dom'
-import {render, screen} from '@testing-library/react'
+import {render, screen, fireEvent} from '@testing-library/react'
 import {renderWithProviders} from '../Redux/test-util'
 import {App} from './App'
 
-test('renders the child component', () => {
+test('renders with default class name', () => {
 	const {container} = renderWithProviders(<App />)
-	expect(container).toBe()
+	const intro = container.getElementsByClassName('intro')
+	expect(intro.length).toBe(1)
+	expect(intro).toBe()
 	})
+test('should change class name if intro is clicked', () => {
+	const {container} = renderWithProviders(<App />)
+	fireEvent.click(screen.getByText(/profile/i))
+	let intro
+	intro = container.getElementsByClassName('intro')
+	expect(intro.length).toBe(0)
+	
+	fireEvent.click(screen.getByText(/intro/i))
+	intro = container.getElementsByClassName('intro')
+	expect(intro.length).toBe(1)
+	})
+test('should change class name if profile is clicked', () => {
+	const {container} = renderWithProviders(<App />)
+	fireEvent.click(screen.getByText(/profile/i))
+	const profile = container.getElementsByClassName('profile')
+	expect(profile.length).toBe(1)
+	})
+test('should change class name if project is clicked', () => {
+	const {container} = renderWithProviders(<App />)
+	fireEvent.click(screen.getByText(/project/i))
+	const profile = container.getElementsByClassName('project')
+	expect(profile.length).toBe(1)
+	})
+test('should change class name if contacts is clicked', () => {
+	const {container} = renderWithProviders(<App />)
+	fireEvent.click(screen.getByText(/contacts/i))
+	const profile = container.getElementsByClassName('contacts')
+	expect(profile.length).toBe(1)
+	})
+
