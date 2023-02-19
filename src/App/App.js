565be2224025f814be72fbe0d2fa7ce1e98fd753
Yaroslav Moroz <ya.moroz.yaroslav@gmail.com>
2023-02-18 20:06:21 -0400

    033

--- src/App/App.js
+++ src/App/App.js
@@ -4,30 +4,31 @@ import {Header} from '../Comps/Header/Header'
 import {Footer} from '../Comps/Footer/Footer'
 import {Router} from '../Router/Router'
 import {useSelector} from 'react-redux'
+import styled from 'styled-components'
 import classnames from 'classnames'
-import {Upper} from '../Comps/Upper/Upper'
+
+const Body = styled.div`
+    height: 100%;
+    width: 101.2%;
+    margin: -8px 0px 0px -8px;
+`
 
 export function App() {
 	
-	
-const selectSection = state => state.open.section
-const section = useSelector(selectSection)
-
-
+const section = useSelector(state => state.open.section)
 
   return (
-    <div className={s.body}>
+    <Body>
       <div className={classnames(section==='intro'&&s.intro,
 		                         section==='profile'&&s.profile,
 		                         section==='project'&&s.project,
 		                         section==='contacts'&&s.contacts
 		                                                              )} > 
        <section className={s.header}><Header /></section>
-       <Upper />
        <section className={s.router}><Router/></section>
        <section className={s.footer}><Footer /></section>
        </div>
-    </div>
+    </Body>
   )
 }
 
