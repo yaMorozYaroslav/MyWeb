4f90a0abba1e1bd693b538eac9ba8144f1f93bc7
Yaroslav Moroz <ya.moroz.yaroslav@gmail.com>
2023-02-21 08:48:31 -0400

    036

--- src/App/App.js
+++ src/App/App.js
@@ -11,6 +11,8 @@ const Body = styled.div`
     height: 100%;
     width: 105%;
     margin: -8px 0px 0px -8px;
+    @media (max-width: 850px){width:103%;}
+    @media (max-width: 500px){width:105%;}
 `
 const BackImg = styled.div`
  ${({current}) => current === 'intro' && `
@@ -27,11 +29,19 @@ const BackImg = styled.div`
    background-image: url(${pics.profile});
    background-position: 0px -270px;
    background-size: cover;
+  @media (max-width: 1250px){background-position: 0px -220px;}
+  @media (max-width: 1000px){background-position: 0px -200px;}
+  @media (max-width: 750px){background-position: -200px -200px;}
+  @media (max-width: 650px){background-position: -500px -400px;;}
+  @media (max-width: 450px){background-position: -450px -300px;}
  `}
  ${({current}) => current === 'project' && `
    background-image: url(${pics.project});
    background-position: 0px -400px;
    background-size: cover;
+  @media (max-width: 1000px){background-position: -100px -200px;}
+  @media (max-width: 850px){background-position: -500px -400px;;}
+  @media (max-width: 450px){background-position: -450px -300px;}
  `}
  ${({current}) => current === 'contacts' && `
    background-image: url(${pics.contacts});
---
+++ src/screen.js
@@ -0,0 +1,19 @@
+const size = {
+  mobS: '350px',
+  mobM: '450px',
+  mobL: '600px',
+  tabl: '850px',
+  lapt: '1000px',
+  lapL: '1250px',
+}
+
+export const device = {
+	
+  mobS: `(min-width: ${size.mobS})`,
+  mobM: `(min-width: ${size.mobM})`,
+  mobL: `(min-width: ${size.mobL})`,
+  tabl: `(min-width: ${size.tabl})`,
+  lapt: `(min-width: ${size.lapt})`,
+  lapL: `(min-width: ${size.lapL})`,
+}
+
