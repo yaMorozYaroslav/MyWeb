0285336960cf2f761fb8c974383df210e2188d4c
Yaroslav Moroz <ya.moroz.yaroslav@gmail.com>
2023-03-21 22:25:33 -0300

    065

--- src/Comps/Alert/Alert.js
+++ src/Comps/Alert/Alert.js
@@ -11,7 +11,7 @@ export const Alert = () => {
 		}
 	return(
 	<section className={s.container}>
-	<h1 className={s.message}>You've sent<br/> the message.</h1>
+	<h1 className={s.message}>You have sent<br/> the message.</h1>
 	<button className={s.button} onClick={double}>Close</button>
 	</section>
 	)
